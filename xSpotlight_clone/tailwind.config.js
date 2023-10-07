75ede43bc72f7ccb504198253475f49c41f5a5e4
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2023-10-07 11:21:13 +0300

    add a part of styling for navbar and change the brakpoints for tailwind (#4)

--- xSpotlight_clone/tailwind.config.js
+++ xSpotlight_clone/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      screens: {
+        sm: "576px", // Small devices (landscape phones, 576px and up)
+        md: "768px", // Medium devices (tablets, 768px and up)
+        lg: "992px", // Large devices (desktops, 992px and up)
+        xl: "1200px", // Extra large devices (large desktops, 1200px and up)
+        "2xl": "1400px", // Extra extra large devices (larger desktops, 1400px and up)
+      },
       colors: {
         dark: "#161616",
         yellow: "#fbca1c",
