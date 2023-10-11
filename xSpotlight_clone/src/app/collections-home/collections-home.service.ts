71124ca922355d38b47c0097018321ccf52b9541
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2023-10-11 17:02:06 +0300

    Collections main (#13)
    * add hero to homepage
    
    * add more styling
    
    * complete hero section
    
    * add fist part of trading comp
    
    * complete tranding list
    
    * add changes
    
    * add first collection
    
    * finish first collection
    
    * add all collections

--- xSpotlight_clone/src/app/collections-home/collections-home.service.ts
+++ xSpotlight_clone/src/app/collections-home/collections-home.service.ts
@@ -1,18 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
 import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CollectionsHomeService {
-
   private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
-  getItem(): Observable<any> {
-    return this.http.get<any>(
-      `${this.apiUrl}/nfts?size=10&search=SUBJECTX-2c184d&withScamInfo=false&computeScamInfo=false`
+  getItems(): Observable<any[]> {
+    // Define your search parameters
+    const searchParams = [
+      'SUBJECTX-2c184d',
+      'COW-cd463d',
+      'BAXC-cdf74d',
+      'MAFIA-bd0abc',
+      'GIANTS-93cadd',
+      'GNOGONS-73222b',
+      'DRIFTERS-efd96c',
+    ];
+
+    const apiCalls = searchParams.map((param) =>
+      this.http.get<any>(
+        `${this.apiUrl}/nfts?size=10&search=${param}&withScamInfo=false&computeScamInfo=false`
+      )
+    );
+
+    return forkJoin(apiCalls).pipe(
+      map((responses) => responses.map((response) => response)) // Assuming each response has an 'items' property
     );
   }
 }
