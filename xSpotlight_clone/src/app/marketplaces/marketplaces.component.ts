fce22d26daa224457da240fb14a72322311365dc
Cucu Toma Daniel <61708051+DanielTomaCucu@users.noreply.github.com>
2023-10-12 08:57:54 +0300

    Marketplaces home (#14)
    * add hero to homepage
    
    * add more styling
    
    * complete hero section
    
    * add fist part of trading comp
    
    * complete tranding list
    
    * add changes
    
    * add first collection
    
    * finish first collection
    
    * add all collections
    
    * add marketplace in home
    
    ---------

---
+++ xSpotlight_clone/src/app/marketplaces/marketplaces.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplaces',
+  templateUrl: './marketplaces.component.html',
+  styleUrls: ['./marketplaces.component.css']
+})
+export class MarketplacesComponent {
+
+}
