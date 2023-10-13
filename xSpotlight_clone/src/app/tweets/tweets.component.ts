4550ae8afb7571014635cfc5e77dd04f0ba9c299
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-10-13 14:09:57 +0300

    add tweets

---
+++ xSpotlight_clone/src/app/tweets/tweets.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-tweets',
+  templateUrl: './tweets.component.html',
+  styleUrls: ['./tweets.component.css'],
+})
+export class TweetsComponent {
+  onLoad(event:any) {
+
+}
+}
