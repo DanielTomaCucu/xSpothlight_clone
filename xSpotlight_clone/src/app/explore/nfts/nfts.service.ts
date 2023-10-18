d5fada0554e52fae56c2b24b615366ec252bfe8e
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-10-18 15:24:09 +0300

    solve last issues

--- xSpotlight_clone/src/app/explore/nfts/nfts.service.ts
+++ xSpotlight_clone/src/app/explore/nfts/nfts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from 'src/app/environments/environment';
 
 @Injectable({
@@ -11,7 +11,12 @@ export class NftsService {
   page: number = 0;
   constructor(private http: HttpClient) {}
   getNfts(): Observable<any> {
-    this.page += 10;
-    return this.http.get<any>(`${this.baseApi}/nfts?from=${this.page}&size=10`);
+    return this.http
+      .get<any>(`${this.baseApi}/nfts?from=${this.page}&size=10`)
+      .pipe(
+        tap(() => {
+          this.page += 10;
+        })
+      );
   }
 }
